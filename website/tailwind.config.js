1790b778fcb499825bf2ebce2695981dba97d232
Sallu6767 <linglanboss2@gmail.com>
2026-08-30 23:08:22 +0600

    Update tailwind.config.js

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                obsidian: "#09090B",
+                elevated: "#18181B",
+                border: "#27272A",
+                muted: "#A1A1AA",
+            },
+        },
+    },
+    plugins: [],
 };
